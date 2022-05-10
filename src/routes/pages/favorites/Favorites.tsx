af4d85b387da8a6941f3104841431df8eafb27a6
bisari <bisari31@gmail.com>
2022-05-10 18:34:53 +0900

    Feat: 즐겨찾기 페이지 초기 셋팅

---
+++ src/routes/pages/favorites/Favorites.tsx
@@ -0,0 +1,11 @@
+import styles from "./Favorites.module.scss"
+
+const Favorites = () => {
+  return (
+    <div className={styles.wrapper}>
+      Favorites
+    </div>
+  )
+}
+
+export default Favorites
