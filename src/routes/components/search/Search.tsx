63f12b279b5d572ab4081ada96e67f10ac88503f
bisari <bisari31@gmail.com>
2022-05-11 15:26:25 +0900

    Refactor: fetch 함수 모듈화

--- src/routes/components/search/Search.tsx
+++ src/routes/components/search/Search.tsx
@@ -1,22 +1,23 @@
-import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { RiSearchLine } from 'react-icons/ri'
 import { movieData } from 'states/movie'
 import styles from './search.module.scss'
 import { useRecoilState } from 'recoil'
+import { getMovieApi } from 'services/movieSearch'
 
 const Search = () => {
   const [inputText, setInputText] = useState('')
   const [movies, setMovies] = useRecoilState(movieData)
+
   const handleChangeText = (e: React.ChangeEvent<HTMLInputElement>) => setInputText(e.currentTarget.value)
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     if (!inputText) return
-    axios
-      .get(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${inputText}&page=${1}`)
+
+    getMovieApi(inputText, 1)
       .then(({ data }) => {
-        // console.log(data)
-        setMovies(data.Search)
+        console.log(data)
+        // setMovies(data.Search)
       })
       .catch((error) => {
         console.log(error)
